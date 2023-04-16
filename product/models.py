5f2d3b7e7e86ce75949b113ec0b426a4842818c2
SulaimanPat <lilsul346@gmail.com>
2023-04-16 19:09:08 +0600

    hw

--- product/models.py
+++ product/models.py
@@ -7,14 +7,30 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    @property
+    def product_count(self):
+        return self.objects.all()
 class Product(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField()
     price = models.FloatField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
 
+    @property
+    def filtered_review_list(self):
+        return self.review_list.filter(stars__gt=3)
+
+    @property
+    def avg_rating(self):
+        lis = [review.stars for review in self.filtered_review_list.all()]
+        return sum(lis) / len(lis)
+
+STAR_CHOICES = ((iterator_, '* ' * iterator_) for iterator_ in range(1, 6))
 class Review(models.Model):
     text = models.TextField(max_length=50)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='review_list')
+    stars = models.IntegerField(default=5, choices=(STAR_CHOICES))
+
--- product/serializers.py
+++ product/serializers.py
@@ -4,14 +4,39 @@ from product.models import Category,Product,Review
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = "id name".split()
+        fields = "id name product_count".split()
 
 class ProductSerializer(serializers.ModelSerializer):
+    category_name = serializers.SerializerMethodField()
+    filtered_review_list = ReviewSerializer(many=True)
     class Meta:
         model = Product
-        fields = "id title description price category".split()
-
+        fields = "all".split()
+    def get_category_name(self, product):
+        try:
+            return product.category.name
+        except:
+            return None
 class ReviewSerializer(serializers.ModelSerializer):
+    product = ProductSerializer
     class Meta:
         model = Review
-        fields = "id text product".split()
+        fields = "id text product stars".split()
+class ProductReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "id title description price category avg_rating".split()
+
+class CategoryValidateSerializer(serializers.Serializer):
+    name = serializers.CharField()
+
+class ProductValidateSerializer(serializers.Serializer):
+    title = serializers.CharField()
+    descriptions = serializers.CharField()
+    price = serializers.IntegerField()
+    category = serializers.IntegerField()
+
+class ReviewValidateSerializer(serializers.Serializer):
+    text = serializers.CharField()
+    product = serializers.IntegerField()
+    stars = serializers.IntegerField()
